b3a6e7e1ed3455fa82b566d4785f9240097b90b0
Mack Straight <mack@discordapp.com>
2022-01-10 11:47:17 -0800

    fix: use SW_SHOW when making top-level windows visible.
    This prevents a problem where a docked window loses its dock state when
    hidden and then shown again.

--- shell/browser/native_window_views.cc
+++ shell/browser/native_window_views.cc
@@ -1645,7 +1645,7 @@ ui::WindowShowState NativeWindowViews::GetRestoredState() {
   if (IsFullscreen())
     return ui::SHOW_STATE_FULLSCREEN;
 
-  return ui::SHOW_STATE_NORMAL;
+  return ui::SHOW_STATE_DEFAULT;
 }
 
 void NativeWindowViews::MoveBehindTaskBarIfNeeded() {
